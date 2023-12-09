0668df3d53481d22ec4e51a881f50cb195195bc2
Nicolas Lazarte <nicolaslv0018@gmail.com>
2023-12-09 13:40:22 -0500

    Résolution de conflits (ticket #0)

--- app/src/main/kotlin/dti/g55/wordle/Wordle.kt
+++ app/src/main/kotlin/dti/g55/wordle/Wordle.kt
@@ -8,6 +8,21 @@ package dti.g55.wordle
  * @throws IllegalArgumentException si le mot cherché ne comporte pas exactement 5 caractères
  */
 class Wordle( motCherché : String ) {
+
+	// Longueur du mot cherché et des essais
+	val LONGUEUR_MOT = 5
+
+	// Les états possibles pour une lettre
+
+	// La lettre n'a pas encore été essayée
+	val ÉTAT_INCONNUE = 0
+	// La lettre n'est pas dans le mot
+	val ÉTAT_ABSENTE = 1
+	// La lettre dans le mot, sa position est connue
+	val ÉTAT_CORRECTE = 2
+	// La lettre dans le mot, sa position est inconnue
+	val ÉTAT_PRÉSENTE = 3
+
 	// Le mot cherché
 	var motCherché : String = ""
 		private set
